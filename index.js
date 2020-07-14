68b352c16c197bcf29f1e4099b31a8986ec525c2
Unko <kobie.watkins@gmail.com>
2020-07-14 03:20:21 -0400

    coding in the basic sections for the README using lessons from class

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const prompt = require("inquirer").createPromptModule();
 const fs = require("fs");
 
-// array of questions for user
-const questions = [];
+const api = require("./utils/api.js");
+const README = require();
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+  fs.writeFile(readMe + ".Markdown", data, (error) =>
+    error
+      ? console.error(error)
+      : console.log(`${fileName + "Markdown"} generated!`)
+  );
+};
 
-// function to initialize program
-function init() {}
+const writeFileAsync = util.promisify(fs.writeFile);
+// array of questions for user
+const questions = [];
+function promptUser() {
+  return inquirer.prompt([]);
+}
 
 // function call to initialize program
 init();
