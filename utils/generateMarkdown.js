51e0851ee929ed93478847867e93e808e65e1b87
Unko <kobie.watkins@gmail.com>
2020-07-16 16:43:58 -0400

    updated code, specifically the license also added the lines part of the crtieria-not sure if that part is correct.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,35 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  # ${data.title.toUpperCase()}
+
+  ![License: ${
+    data.lic ? data.lic : "None"
+  }](https://img.shields.io/badge/License-MIT-yellow.svg-${
+    data.lic ? data.lic : "None"
+  }-brightgreen)
+
+  _Repo by ${data.name}_
+  
+  __${data.desc}__
+
+  ---
+ __Installation:__
+  ${data.import
+    
+    __Usage:__
+  $data.use
+
+  __Contributors:__
+  $data.con
+
+    __Tests:__
+  $data.test
+
+  __Questions:__
+  $data.questions
+
+  {  } from "module":}
   
   `;
 }
